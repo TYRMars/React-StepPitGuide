d6552a399de38310a06075e00342257178d1b4a4
Jonathan Zhang <zhangjianan1996@qq.com>
2017-10-28 15:30:01 +0800

    update

--- 02-Router4/src/Dashboard.js
+++ 02-Router4/src/Dashboard.js
@@ -30,6 +30,7 @@ class Dashboard extends React.Component {
         super(props)
     }
     render() {
+      const match = this.props.match;
       console.log(this.props);
       const redirectToLogin = <Redirect to='/login'></Redirect>
       const app = (
@@ -38,18 +39,18 @@ class Dashboard extends React.Component {
           {this.props.isAuths ? null : <button onClick={this.props.logout}>注销</button>}
               <ul>
                   <li>
-                      <Link to='/dashboard/'>首页</Link>
+                      <Link to={`${match.path}/`}>首页</Link>
                   </li>
                   <li>
-                      <Link to='/dashboard/news'>新闻</Link>
+                      <Link to={`${match.path}/news`}>新闻</Link>
                   </li>
                   <li>
-                      <Link to='/dashboard/video'>视频</Link>
+                      <Link to={`${match.path}/video`}>视频</Link>
                   </li>
               </ul>
-              <Route path='/dashboard/' exact component={App}></Route>
-              <Route path='/dashboard/news' component={News}></Route>
-              <Route path='/dashboard/video' component={Video}></Route>
+              <Route path={`${match.path}/`} exact component={App}></Route>
+              <Route path={`${match.path}/news`} component={News}></Route>
+              <Route path={`${match.path}/video`} component={Video}></Route>
           </div>
       )
         return this.props.isAuth ? app: redirectToLogin
